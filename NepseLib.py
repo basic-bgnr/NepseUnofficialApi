575867f3d521cc517dd9c611a40e4aaaec1c0d28
basic-bgnr <devkotabidit@gmail.com>
2022-01-11 09:26:48 +0545

    added post method payload decoder for accessing today_price api

--- NepseLib.py
+++ NepseLib.py
@@ -2,6 +2,8 @@ import requests
 from collections import defaultdict
 from json import JSONDecodeError
 import json
+import time
+from datetime import date
 
 class TokenParser():
     def __init__(self):
@@ -125,6 +127,9 @@ class Nepse:
                                 "nepse_open_url"       : f"{self.base_url}/api/nots/nepse-data/market-open",
                                 "nepse_index_url"      : f"{self.base_url}/api/nots/nepse-index",
                                 "nepse_subindices_url" : f"{self.base_url}/api/nots",
+                                "nepse_isopen"         : f"{self.base_url}/api/nots/nepse-data/market-open",
+
+                                "todays_price"         : f"{self.base_url}/api/nots/nepse-data/today-price?&size=20&securityId=2742&businessDate=2022-01-06",
                               }
         
         self.api_end_point_access_token = defaultdict(lambda : False)
@@ -158,6 +163,21 @@ class Nepse:
             return self.requestAPI(url) 
         
         return response.json()
+    
+    def requestPOSTAPI(self, url):
+        self.incrementTotalRequestCount()
+        
+        if url in self.api_end_points.values():
+            access_token, request_token = self.getTokenForURL(url)
+            
+            headers = {'Content-Type':'application/json', 'Authorization': f'Salter {access_token}', **self.headers, }
+            response = requests.post(url, headers=headers, data=json.dumps({"id": self.getPOSTPayloadID()}))
+            
+            if (response.status_code != 200):
+                self.refreshTokenForURL(url)
+                return self.requestAPI(url)
+            
+            return response.json()
             
     
     #token is unique for each url, when token is requested, the access token received when first used for accessing a url can be 
@@ -213,8 +233,30 @@ class Nepse:
         
     def incrementTotalRequestCount(self):
         self.total_request_count += 1
+
+    ##################method to get post payload id#################################33
+    def getDummyID(self):
+        return self.getMarketStatus()['id']
+    
+    def getDummyData(self):
+        return [147, 117, 239, 143, 157, 312, 161, 612, 512, 804, 411, 527, 170,
+            511, 421, 667, 764, 621, 301, 106, 133, 793, 411, 511, 312, 423,
+            344, 346, 653, 758, 342, 222, 236, 811, 711, 611, 122, 447, 128,
+            199, 183, 135, 489, 703, 800, 745, 152, 863, 134, 211, 142, 564,
+            375, 793, 212, 153, 138, 153, 648, 611, 151, 649, 318, 143, 117,
+            756, 119, 141, 717, 113, 112, 146, 162, 660, 693, 261, 362, 354,
+            251, 641, 157, 178, 631, 192, 734, 445, 192, 883, 187, 122, 591,
+            731, 852, 384, 565, 596, 451, 772, 624, 691,
+          ]
+    
+    def getPOSTPayloadID(self):
+        dummy_id = self.getDummyID()
+        return self.getDummyData()[dummy_id] + dummy_id + 2*(date.today().day)
     
     ###############################################PUBLIC METHODS###############################################
+    def getMarketStatus(self):
+        return self.requestAPI(url=self.api_end_points['nepse_isopen'])
+
     def getTotalRequestCount(self):
         return self.total_request_count
     
