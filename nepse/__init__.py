2a51371b2d6e218e00112862ead3b1fd9ff14ce9
basic-bgnr <devkotabidit@gmail.com>
2024-09-14 20:22:39 +0545

    changed version and release date

--- nepse/__init__.py
+++ nepse/__init__.py
@@ -14,8 +14,8 @@ __all__ = [
     "AsyncNepse",
 ]
 
-__version__ = "0.3.4"
-__release_date__ = timestamp(2024, 6, 24)
+__version__ = "0.3.5.dev0"
+__release_date__ = timestamp(2024, 9, 14)
 
 
 def main_cli():
