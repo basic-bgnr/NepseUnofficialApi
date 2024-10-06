3448595eec133f9babd87188087bf143a3beabf6
Aakash Basnet <iamaakashbasnet@gmail.com>
2024-10-06 12:42:24 +0545

    refactor: fix f string splitting

--- nepse/NepseLib.py
+++ nepse/NepseLib.py
@@ -157,8 +157,7 @@ class _Nepse:
 
     # api requiring post method
     def getPriceVolumeHistory(self, business_date=None):
-        url = f"{self.api_end_points['todays_price']
-                 }?&size=500&businessDate={business_date}"
+        url = f"{self.api_end_points['todays_price']}?&size=500&businessDate={business_date}"
         return self.requestPOSTAPI(
             url=url, payload_generator=self.getPOSTPayloadIDForFloorSheet
         )
@@ -408,8 +407,7 @@ class AsyncNepse(_Nepse):
         start_date = start_date if start_date else (end_date - timedelta(days=365))
         symbol = symbol.upper()
         company_id = (await self.getSecurityIDKeyMap())[symbol]
-        url = f"{self.api_end_points['company_price_volume_history']}{
-            company_id}?&size=500&startDate={start_date}&endDate={end_date}"
+        url = f"{self.api_end_points['company_price_volume_history']}{company_id}?&size=500&startDate={start_date}&endDate={end_date}"
         return (await self.requestGETAPI(url=url))["content"]
 
     # api requiring post method
@@ -431,8 +429,7 @@ class AsyncNepse(_Nepse):
 
     async def getFloorSheet(self, show_progress=False):
 
-        url = f"{self.api_end_points['floor_sheet']}?&size={
-            self.floor_sheet_size}&sort=contractId,desc"
+        url = f"{self.api_end_points['floor_sheet']}?&size={self.floor_sheet_size}&sort=contractId,desc"
         sheet = await self.requestPOSTAPI(
             url=url, payload_generator=self.getPOSTPayloadIDForFloorSheet
         )
@@ -626,8 +623,7 @@ class Nepse(_Nepse):
         start_date = start_date if start_date else (end_date - timedelta(days=365))
         symbol = symbol.upper()
         company_id = self.getSecurityIDKeyMap()[symbol]
-        url = f"{self.api_end_points['company_price_volume_history']}{
-            company_id}?&size=500&startDate={start_date}&endDate={end_date}"
+        url = f"{self.api_end_points['company_price_volume_history']}{company_id}?&size=500&startDate={start_date}&endDate={end_date}"
         return self.requestGETAPI(url=url)
 
     # api requiring post method
@@ -674,8 +670,7 @@ class Nepse(_Nepse):
         business_date = (
             date.fromisoformat(f"{business_date}") if business_date else date.today()
         )
-        url = f"{self.api_end_points['company_floorsheet']}{company_id}?&businessDate={
-            business_date}&size={self.floor_sheet_size}&sort=contractid,desc"
+        url = f"{self.api_end_points['company_floorsheet']}{company_id}?&businessDate={business_date}&size={self.floor_sheet_size}&sort=contractid,desc"
         sheet = self.requestPOSTAPI(
             url=url, payload_generator=self.getPOSTPayloadIDForFloorSheet
         )
